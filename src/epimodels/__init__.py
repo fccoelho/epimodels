ccb374461e9b6b4b53d1e0b1e5fa5eb69cd5e931
Flávio Codeço Coelho <fccoelho@gmail.com>
2020-05-18 12:49:32 -0300

    restored Basemodel Code

--- src/epimodels/__init__.py
+++ src/epimodels/__init__.py
@@ -9,3 +9,29 @@ except DistributionNotFound:
     __version__ = 'unknown'
 finally:
     del get_distribution, DistributionNotFound
+
+from matplotlib import pyplot as P
+
+class BaseModel:
+    """
+    Base class for all models both discrete and continuous
+    """
+    def __init__(self):
+        self.name = None
+        self.model_type = None
+        self.state_variables = {}
+        self.parameters = {}
+        self.traces = {}
+
+    def plot_traces(self, vars: list=[]):
+        """
+        Plots the simulations
+        :param vars: variables to plot
+        """
+        for series, data in self.traces.items():
+            if series in vars:
+                P.plot(self.traces['time'], data, label=series)
+        P.legend(loc=0)
+        P.grid()
+        P.title("{} model".format(self.model_type))
+
