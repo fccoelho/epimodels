088fa5f3a1a97b0ab5a2dd3ee4bf26c5720f55c3
Flávio Codeço Coelho <fccoelho@gmail.com>
2021-03-14 10:24:07 -0300

    Added conda recipe

--- setup.py
+++ setup.py
@@ -3,17 +3,16 @@
     Setup file for epimodels.
     Use setup.cfg to configure your project.
 
-    This file was generated with PyScaffold 3.2.3.
+    This file was generated with PyScaffold 3.3.1.
     PyScaffold helps you to put up the scaffold of your new Python project.
     Learn more under: https://pyscaffold.org/
 """
 import sys
-
 from pkg_resources import VersionConflict, require
 from setuptools import setup
 
 try:
-    require('setuptools>=38.3')
+    require("setuptools>=38.3")
 except VersionConflict:
     print("Error: version of setuptools is too old (<38.3)!")
     sys.exit(1)
--- src/epimodels/__init__.py
+++ src/epimodels/__init__.py
@@ -1,41 +1,11 @@
 # -*- coding: utf-8 -*-
-from pkg_resources import get_distribution, DistributionNotFound
+from pkg_resources import DistributionNotFound, get_distribution
 
 try:
     # Change here if project is renamed and does not equal the package name
     dist_name = __name__
     __version__ = get_distribution(dist_name).version
 except DistributionNotFound:
-    __version__ = 'unknown'
+    __version__ = "unknown"
 finally:
     del get_distribution, DistributionNotFound
-
-from matplotlib import pyplot as P
-
-
-class BaseModel:
-    """
-    Base class for all models both discrete and continuous
-    """
-
-    def __init__(self):
-        self.name = None
-        self.model_type = None
-        self.state_variables = {}
-        self.parameters = {}
-        self.traces = {}
-
-    def plot_traces(self, vars: list = []):
-        """
-        Plots the simulations
-        :param vars: variables to plot
-        """
-        if vars == []:  # Plot all
-            vars = [s for s in self.traces if s != 'time']
-
-        for series, data in self.traces.items():
-            if series in vars:
-                P.plot(self.traces['time'], data, label=series)
-        P.legend(loc=0)
-        P.grid()
-        P.title("{} model".format(self.model_type))
